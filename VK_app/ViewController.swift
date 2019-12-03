8b948cbe9df5f1f8a84a384099079c39cfb14625
MaxSwift19 <maxsthm@mail.ru>
2019-12-03 08:02:38 +0300

    Initial Commit

---
+++ VK_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK_app
+//
+//  Created by User on 03/12/2019.
+//  Copyright © 2019 Home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
